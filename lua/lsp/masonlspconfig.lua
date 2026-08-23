7dd0d2395722ec437f37a6ad9987aa4ad1d39bac
Widua <mikolaj.widla@gmail.com>
2026-08-23 15:07:23 +0200

    Blink upgrade, and mason simplyfy

--- lua/lsp/masonlspconfig.lua
+++ lua/lsp/masonlspconfig.lua
@@ -21,35 +21,18 @@ local servers = {
   },
 }
 
-local ensure_installed = vim.tbl_keys(servers or {})
-vim.list_extend(ensure_installed, {
-  'stylua',
-})
-
 require('mason-tool-installer').setup {
-  ensure_installed = ensure_installed,
+  ensure_installed = { 'stylua' },
 }
 
+for server_name, server in pairs(servers) do
+  server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+  vim.lsp.config(server_name, server)
+end
+
+local ensure_installed = vim.tbl_keys(servers)
+
 require('mason-lspconfig').setup {
-  automatic_enable = {
-    exclude = {
-      'jdtls',
-    },
-  },
-  ensure_installed = {},
-  automatic_installation = false,
-
-  handlers = {
-    function(server_name)
-      local server = servers[server_name] or {}
-
-      server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-
-      if not vim.lsp.get_config(server_name) then
-        vim.lsp.config(server_name, server)
-      end
-
-      vim.lsp.enable(server_name)
-    end,
-  },
+  ensure_installed = ensure_installed,
+  automatic_enable = ensure_installed,
 }
--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -10,7 +10,18 @@ blink.setup {
   },
 
   completion = {
-    documentation = { auto_show = false, auto_show_delay_ms = 500 },
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 500,
+      treesitter_highlighting = true,
+      window = {
+        max_width = 80,
+        max_height = 20,
+        border = 'rounded',
+        winblend = 5,
+        scrollbar = true,
+      },
+    },
   },
 
   sources = {
@@ -26,7 +37,7 @@ blink.setup {
 
   snippets = { preset = 'luasnip' },
 
-  fuzzy = { implementation = 'lua' },
+  fuzzy = { implementation = 'prefer_rust_with_warning' },
 
   signature = { enabled = true },
 }
